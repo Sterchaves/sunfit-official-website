e4211a73e3a1601d1853264a339e3104a07fc976
Stephanny Chaves <stefannychaves1001@gmail.com>
2024-09-27 17:47:19 -0300

    carousel

--- script/script.js
+++ script/script.js
@@ -1,3 +1,32 @@
+// SWIPER
+// Tranding Product Slider-start
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// Tranding Product Slider-end
+
+
+
+
+
 const itensPerguntasERespostas = document.querySelectorAll(".category");
 
 itensPerguntasERespostas.forEach(function(item) {
@@ -24,6 +53,5 @@ itensPerguntasERespostas.forEach(function(item) {
         }
     });
 });
-
 
 
